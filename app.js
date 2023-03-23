5a6a77f18597e67385d9345da40581395c8e02db
Mariooooo <lintt74123@gmail.com>
2023-03-23 23:48:06 +0800

    Mongoose create practice

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 app.set("view engine", "ejs");
 
@@ -8,6 +9,36 @@ mongoose
     .connect("mongodb://127.0.0.1:27017/exampleDB") //Need to use 127.0.0.1,localhost might defeat.
     .then(() => {
         console.log("Connect mongoDB successfully...");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+const studentSchema = new Schema({
+    name: String, //{type:String}
+    age: Number,
+    major: String,
+    money: {
+        TWD: Number,
+        USD: Number,
+    },
+});
+
+const Student = mongoose.model("Student", studentSchema);
+const newObj = new Student({
+    name: "Roy",
+    age: 30,
+    major: "Math",
+    money: {
+        TWD: 6000,
+        USD: 7000,
+    },
+});
+newObj
+    .save()
+    .then((saveObj) => {
+        console.log("Data has been storage");
+        console.log(saveObj);
     })
     .catch((e) => {
         console.log(e);
